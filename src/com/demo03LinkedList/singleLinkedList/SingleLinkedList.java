cb0b17d906e0f46a0aeb88d688f783de8e786ddf
zhaojingchao <2356430623@qq.com>
2019-10-27 19:13:25 +0800

    单链表实现增删改查

--- src/com/demo03LinkedList/singleLinkedList/SingleLinkedList.java
+++ src/com/demo03LinkedList/singleLinkedList/SingleLinkedList.java
@@ -1,5 +1,7 @@
 package com.demo03LinkedList.singleLinkedList;
 
+import org.w3c.dom.html.HTMLTableRowElement;
+
 /**
  * @author : 赵静超
  * @date Date : 2019/10/22 21:42
@@ -9,8 +11,6 @@ public class SingleLinkedList {
 
     //先定义一个头结点，头结点不动，不存放任何数据
     private HeroNode head = new HeroNode(0,"","");
-
-    //
 
     /**
      * 添加节点到单链表中
@@ -31,6 +31,99 @@ public class SingleLinkedList {
         }
         //当退出while循环时，将之前链表内最后一个节点指向新添加的节点
         temp.next=node;
+    }
+
+    /**
+     * 根据节点的编号添加到链表的指定位置，如果有该编号则抛出异常
+     * @param node
+     */
+    public void addByOrder(HeroNode node){
+        //因为头结点不能动，因此需要建立临时节点，来帮助我们找到添加的位置(插入位置的前一个节点)
+        HeroNode temp = head;
+        //建立flag表示添加的节点在原链表中是否存在，默认不存在 false
+        boolean flag = false;
+        while(true){
+            //temp节点已经移动到链表的最后
+            if(temp.next==null){
+                break;
+            }
+            //已经找到插入的位置，就在temp的后面插入节点即可
+            if(temp.next.id>node.id){
+                break;
+            }else if(temp.next.id == node.id){ //新插入的节点在原单链表中已经存在
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(flag){
+            System.out.printf("您要插入的%d号节点已经存在于链表中\n",node.id);
+            return ;
+        }else{
+            node.next = temp.next;
+            temp.next = node;
+        }
+    }
+
+    /**
+     * 根据ID删除节点
+     * 建立临时节点temp指向要删除节点的前一个节点
+     * @param id
+     */
+    void del(int id){
+        if(head.next == null){
+            System.out.println("当前链表为空，不能进行删除操作!");
+            return;
+        }
+        HeroNode temp = head;
+        boolean flag =false;     //表示有没有找到要删除节点的前一个节点，默认为false
+        while (true){
+            if(temp.next == null){      //已经遍历到链表尾部
+                break;
+            }
+            if(temp.next.id == id){     //找到要删除的节点
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if(flag){       //找到要删除得节点
+            temp.next = temp.next.next;
+            System.out.printf("删除%d号节点成功！\n",id);
+        }else{
+            System.out.printf("要删除的%d节点不存在！\n",id);
+        }
+    }
+
+    /**
+     * 根据新节点id来修改找原节点，修改其信息
+     * @param newHeroNode
+     */
+    public void update(HeroNode newHeroNode){
+        //判断链表是否为空
+        if(head.next == null){
+            System.out.println("链表为空，不能进行修改操作！");
+            return;
+        }
+        HeroNode temp = head.next;
+        boolean flag = false;   //表示是否找到该节点
+        while (true){
+            if(temp == null){    //遍历到链表结尾
+                break;
+            }
+            if(newHeroNode.id == temp.id){     //找到要修改的节点
+                flag =true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //判断是否找到了需要修改的节点
+        if(flag){
+            temp.name = newHeroNode.name;
+            temp.company = newHeroNode.company;
+        }else{
+            System.out.printf("没有找到编号为%d的节点，不能进行修改",newHeroNode.id);
+        }
     }
 
     /**
--- src/com/demo03LinkedList/singleLinkedList/SingleLinkedListDemo.java
+++ src/com/demo03LinkedList/singleLinkedList/SingleLinkedListDemo.java
@@ -8,18 +8,38 @@ package com.demo03LinkedList.singleLinkedList;
 public class SingleLinkedListDemo {
 
     public static void main(String[] args) {
-        HeroNode hero1 = new HeroNode(0, "任正非", "华为公司");
-        HeroNode hero2 = new HeroNode(1, "马化腾", "腾讯公司");
-        HeroNode hero3 = new HeroNode(2, "李彦宏", "百度公司");
-        HeroNode hero4 = new HeroNode(3, "刘强东", "京东公司");
+        HeroNode hero1 = new HeroNode(1, "任正非", "华为公司");
+        HeroNode hero2 = new HeroNode(2, "马化腾", "腾讯公司");
+        HeroNode hero3 = new HeroNode(3, "李彦宏", "百度公司");
+        HeroNode hero4 = new HeroNode(4, "刘强东", "京东公司");
 
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.add(hero1);
+       /*singleLinkedList.add(hero1);
         singleLinkedList.add(hero2);
         singleLinkedList.add(hero3);
-        singleLinkedList.add(hero4);
+        singleLinkedList.add(hero4);*/
+
+       //按照编号顺序添加节点
+        singleLinkedList.addByOrder(hero1);
+        singleLinkedList.addByOrder(hero4);
+        singleLinkedList.addByOrder(hero3);
+        singleLinkedList.addByOrder(hero2);
 
         singleLinkedList.getList();
 
+        //测试修改节点信息方法
+        HeroNode newHeroNode = new HeroNode(2, "小马", "腾讯股份有限公司");
+        singleLinkedList.update(newHeroNode);
+        System.out.println("修改之后的单链表");
+        singleLinkedList.update(newHeroNode);
+        singleLinkedList.getList();
+
+        //测试删除节点方法
+        singleLinkedList.del(4);
+        singleLinkedList.del(1);
+        singleLinkedList.del(3);
+        singleLinkedList.del(2);
+        System.out.println("删除后的链表信息");
+        singleLinkedList.getList();
     }
 }
